4cfd5e1c279ab6c3c6339ab6f2bb6811243a00c6
Admenri Chen <admenri0504@163.com>
2024-01-09 23:11:27 +0800

    增加工程自动生成配置文件

---
+++ components/fpslimiter/fpslimiter.cc
@@ -0,0 +1,9 @@
+// Copyright 2024 Admenri.
+// Use of this source code is governed by a BSD - style license that can be
+// found in the LICENSE file.
+
+#include "components/fpslimiter/fpslimiter.h"
+
+#include "base/debug/logging.h"
+
+namespace fpslimiter {}
---
+++ components/fpslimiter/fpslimiter.h
@@ -0,0 +1,10 @@
+// Copyright 2024 Admenri.
+// Use of this source code is governed by a BSD - style license that can be
+// found in the LICENSE file.
+
+#ifndef COMPONENTS_FPSLIMITER_FPSLIMITER_H_
+#define COMPONENTS_FPSLIMITER_FPSLIMITER_H_
+
+namespace fpslimiter {}
+
+#endif  // !COMPONENTS_FPSLIMITER_FPSLIMITER_H_
