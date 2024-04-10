290e641e72da158083453fc4296bdeab73079e88
Admenri Chen <admenri0504@163.com>
2024-04-10 16:57:42 +0800

    移除settingsmenu残留

--- components/settingsmenu/settings_menu.cc
+++
@@ -1,9 +0,0 @@
-// Copyright 2024 Admenri.
-// Use of this source code is governed by a BSD - style license that can be
-// found in the LICENSE file.
-
-#include "components/settingsmenu/settings_menu.h"
-
-#include "SDL_video.h"
-
-namespace settings {}
